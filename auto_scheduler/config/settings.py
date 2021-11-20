9c98285fc9294b13a314a12f350e4e4e6852e954
Kelly Wu <kelly.wu@edu.uwaterloo.ca>
2021-11-19 21:26:10 -0500

    initial change

--- auto_scheduler/config/settings.py
+++ auto_scheduler/config/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
-    'frontend'
+    'rest_framework',
+    'frontend',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.UserAccount'
